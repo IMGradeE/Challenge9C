b759883bac58dec95833660ecadcecb3578ab6c8
IMGradeE <112536901+IMGradeE@users.noreply.github.com>
2024-09-25 14:49:47 -0600

    I don't know how to do anything anymore I guess.

--- circularBuffer.h
+++ circularBuffer.h
@@ -4,8 +4,11 @@
 
 #ifndef CHALLENGE9_CIRCULARBUFFER_H
 #define CHALLENGE9_CIRCULARBUFFER_H
-
+#include <stdbool.h>
 #include <malloc.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 
 typedef struct circularlyLinkedList {
     void* next;
@@ -13,9 +16,16 @@ typedef struct circularlyLinkedList {
     int data;
 }circularlyLinkedList;
 
+extern void printThreadAction(circularlyLinkedList* item, bool producer);
+
+extern void printDivider(){
+    printf("========================================\n");
+}
+
 /*Always call the cleanup function cleanupCircularBuffer() from this header on each reference returned from this function.*/
 extern circularlyLinkedList* initializeCircularBuffer(int bufferSize){
-
+    printf("Creating Circular Buffer with Size: %d\n", bufferSize);
+    printDivider();
     circularlyLinkedList* lag;
     circularlyLinkedList* first;
     lag = malloc(sizeof (circularlyLinkedList));
@@ -23,10 +33,12 @@ extern circularlyLinkedList* initializeCircularBuffer(int bufferSize){
     lag->data = -1;
     lag->index = 0;
     first = lag;
+    printf("Buffer Index; %d, Data %d\n", lag->index, lag->data);
     for (int i = 1; i < bufferSize; ++i) {
         circularlyLinkedList* curr =  malloc(sizeof (circularlyLinkedList));
         curr->data = -1;
         curr->index = i;
+        printf("Buffer Index; %d, Data %d\n", curr->index, curr->data);
         lag->next = curr;
         lag = curr;
     }
@@ -41,4 +53,43 @@ extern void cleanupCircularBuffer(circularlyLinkedList* base, circularlyLinkedLi
     free(next);
 }
 
+extern void bufferConsume(circularlyLinkedList* baseBuffer, sem_t lock, int* transactionLength){
+    circularlyLinkedList* curr = baseBuffer;
+    while (*transactionLength > 0 ) {
+        sem_wait(&lock);
+        circularlyLinkedList* curr = baseBuffer;
+        while( *transactionLength > 0){
+            if (curr->data != -1) {
+                printThreadAction(curr, false);
+                curr->data = -1;
+                *transactionLength -= 1;
+                curr = curr->next;
+            } else {
+                sem_post(&lock);
+            }
+        }
+    }
+}
+
+extern void bufferProduce(circularlyLinkedList* baseBuffer, int burstLength, sem_t lock, int* transactionLength){
+    circularlyLinkedList* curr = baseBuffer;
+    while (*transactionLength > 0 ){
+        sem_wait(&lock);
+        for (int i = 0; i < burstLength; ++i) {
+            curr->data = rand()%100; // not actually random, should generate the same values each run.
+            printThreadAction(curr, true);
+            *transactionLength -= 1;
+            curr = curr->next;
+        }
+        sem_post(&lock);
+    }
+}
+
+
+extern void printThreadAction(circularlyLinkedList* item, bool producer){
+    char* string = (producer == true)? "Producer Produced": "Consumer Consumed";
+    static int transactionNumber = 0;
+    printf("Sequence #: %d Buffer Index %d, %s %d\n", transactionNumber, item->index, string, item->data);
+    ++transactionNumber;
+}
 #endif //CHALLENGE9_CIRCULARBUFFER_H
--- main.c
+++ main.c
@@ -1,6 +1,57 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <semaphore.h>
 #include "circularBuffer.h"
+#include <unistd.h>
+
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+sem_t lock;
+
+typedef struct {
+    circularlyLinkedList* buffer;
+    int burstLength;
+    int* transactionLength;
+}args;
+
+void* consume(void * arg){
+    args* send = (args*) arg;
+    circularlyLinkedList* curr = send->buffer;
+    while (*send->transactionLength > 0 ) {
+        sem_wait(&lock);
+        sleep(1);
+        if (curr->data != -1) {
+            printThreadAction(curr, false);
+            curr->data = -1;
+            *send->transactionLength -= 1;
+            curr = curr->next;
+        } else {
+            sem_post(&lock);
+        }
+    }
+    /*bufferConsume(send->buffer, lock, send->transactionLength);*/
+}
+
+void* produce(void* arg){
+    srand(0);
+    args* send = (args*) arg;
+    circularlyLinkedList* curr = send->buffer;
+    while (*send->transactionLength > 0 ){
+        sem_wait(&lock);
+        sleep(1);
+        for (int i = 0; i < send->burstLength; ++i) {
+            curr->data = rand()%100; // not actually random, should generate the same values each run.
+            printThreadAction(curr, true);
+            *send->transactionLength -= 1;
+            curr = curr->next;
+        }
+        sem_post(&lock);
+    }
+    /*bufferProduce(send->buffer, send->burstLength, lock, send->transactionLength);*/
+}
 
 int main(void) {
     int bufferLength, burstLength, transactionLength;
@@ -18,11 +69,30 @@ int main(void) {
     // call initialization function
     circularlyLinkedList* buffer = initializeCircularBuffer(bufferLength);
 
+    sem_init(&lock, 0,1);
+
+    // forward declaration of threads
+    pthread_t producer, consumer;
+
+    // Set args
+    args* arguments = malloc(sizeof (args));
+    arguments->transactionLength = &transactionLength;
+    arguments->buffer = buffer;
+    arguments->burstLength = burstLength;
+
     // start threads
-    pthread_t producer;
-    pthread_t consumer;
+    printf("Starting threads");
+    printDivider();
+    pthread_create(&consumer, NULL, consume, (void *) arguments);
+    pthread_create(&producer, NULL, produce, (void *) arguments);
+
+    // join threads
+    pthread_join(producer, NULL);
+    pthread_join(consumer, NULL);
 
     // cleanup
+    sem_destroy(&lock);
+    free(arguments);
     cleanupCircularBuffer(buffer, buffer->next);
     return 0;
 }
